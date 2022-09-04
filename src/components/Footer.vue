98c55c74b06f61bdbc8742c707d340b49ee0b98f
Uvacoder <88749643+Uvacoder@users.noreply.github.com>
2022-09-03 21:43:51 -0400

    Update Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,10 +3,10 @@
     class="flex flex-col md:flex-row justify-between items-center px-4 md:px-6 py-6 text-gray-700 bg-gray-300"
   >
     <div class="mb-2 md:mb-0 md:mr-2">
-      An app by <a href="http://benoitburgener.ch/">LeBen</a>
+      An app by <a href="http://abclinks.vercel.app/">uvacoder</a>
     </div>
     <div>
-      Browse <a href="https://github.com/LeBenLeBen/palette">the code</a> on
+      Browse <a href="https://github.com/uvacoder/abc-color-palettes">the code</a> on
       GitHub
     </div>
   </footer>
